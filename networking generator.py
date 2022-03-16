433b6af2ed84a3baf3c4623b4d6a05e3624569c4
Joewelc <101085294+Joewelc@users.noreply.github.com>
2022-03-16 16:08:14 -0700

    Update networking generator.py

--- networking generator.py
+++ networking generator.py
@@ -17,8 +17,8 @@ if userinput == ("stp"):
 	print(stp)
 
 if userinput == ("ip"):
-	subnet = input("Insert your ip subnet ")											# Ip input
-	submask = input("What is the subnet mask? 0, 128, 64, 32, 16, etc ")				# Subnet input
+	subnet = input("Insert your ip subnet ")	#Sets ip on an interface (Assumes a /24 or more)
+	submask = input("What is the subnet mask? 0, 128, 64, 32, 16, etc ")	# Subnet input
 	if int(submask) == (128):
 		print("ip address " + str(subnet) + " 255.255.255.128")
 	elif int(submask) == (64):
@@ -81,29 +81,29 @@ if userinput == ("duplex"):
 	print(duplex)
 
 if userinput == ("help"):	#Shows current available commands
-	help = ("The commands include: ospf, ip, stp, vlans, passwords, port security, cdp, rip, speed ")
+	help = ("The commands include: ospf, ip, stp, vlans, passwords, port security, cdp, rip, speed, duplex ")
 	print(help)
 
 def main():		#Main loop starts
 	userinput = input("I can help with Cisco commands, type 'help', or insert command: ").lower() 
 	err = ("NULL") 																			
 
-	if userinput == ("ospf"):
+	if userinput == ("ospf"):	
 		ospfval = input("Insert an OSPF process number ")									
 		ospf_area_ask = input("Insert ospf area ")											
 		ospf_area = ("area " + ospf_area_ask)
 		ospf = ("Enable" "\n" "configure terminal" "\n" "router ospf " + ospfval + "\n" "network *IP subnet and wildcard mask* " + ospf_area)
 		print(ospf)
 		
-	if userinput == ("rip"):
+	if userinput == ("rip"): 	
 		rip = ("Enable" "\n" "configure terminal" "\n" "router rip " "\n" "network *IP subnet and mask* " "\n" "version 2")
 		print(rip)
 
-	if userinput == ("stp"):
+	if userinput == ("stp"): 	
 		stp = ("Enable" "\n" "configure terminal" "\n" "spanning-tree mode rapid-pvst" "\n" "spanning tree vlan 1 root primary")
 		print(stp)
 
-	if userinput == ("ip"):
+	if userinput == ("ip"):	
 		subnet = input("Insert your ip subnet ")											
 		submask = input("What is the subnet mask? 128, 64, 32, 16, etc ")					
 		if int(submask) == (128):
@@ -123,12 +123,12 @@ def main():		#Main loop starts
 		elif int(submask) == (0):
 			print("ip address " + str(subnet) + " 255.255.255.0")
 
-	if userinput == ("vlan"):
+	if userinput == ("vlan"):	
 		vlan_num = input("Insert a Vlan number ")											
 		vlan = ("Enable" "\n" "configure terminal" "\n" "vlan " + vlan_num + "\n" "interface */*" "\n" "switchport mode access" "\n" "switchport access vlan " + vlan_num)
 		print(vlan)
 
-	if userinput == ("password"):
+	if userinput == ("password"): 
 		pass_input = input("Insert your password ")											
 		secret_ask = input("Do you want a secret password? Y/N ").lower()
 		if secret_ask == str("y"):
@@ -139,7 +139,7 @@ def main():		#Main loop starts
 		password = ("Enable" "\n" "configure terminal" "\n" "enable password " + str(pass_input) + "\n" + str(secret_enable) + "service password-encryption")
 		print(password)
 
-	if userinput == ("cdp"):
+	if userinput == ("cdp"): 
 		cdp = input("Run or disable CDP? ").lower() 										
 		if cdp == str("run"):
 			cdp = ("cdp run" "\n" "Note: CDP is enabled by default")
@@ -147,7 +147,7 @@ def main():		#Main loop starts
 			cdp = ("no cdp run")
 		print("Enable" "\n" "configure terminal" "\n" + cdp)
 
-	if userinput == ("port security"):
+	if userinput == ("port security"):	
 		port_sec_max = input("Max number of mac addresses allowed: ") 						
 		port_sec_maxmac = ("switchport port-security maximum " + str(port_sec_max))
 		port_sec_opt = input("Mac or Sticky? ").lower()										
@@ -159,16 +159,16 @@ def main():		#Main loop starts
 		port_security = ("Enable" "\n" "configure terminal" "\n" "interface */*" "\n" "switchport mode access" "\n" "switchport port-security " "\n" + port_sec_maxmac + "\n" + str(port_sec_mac))
 		print(port_security)
 
-	if userinput == ("speed"):
+	if userinput == ("speed"):	
 		speed = ("Enable" "\n" "configure terminal" "\n" "interface */*" "\n" "speed 100")
 		print(speed)
 
-	if userinput == ("duplex"):
+	if userinput == ("duplex"):	
 		duplex = ("Enable" "\n" "configure terminal" "\n" "interface */*" "duplex full")
 		print(duplex)
 
-	if userinput == ("help"):						#Shows current available commands
-		help = ("The commands include: ospf, ip, stp, vlans, passwords, port security, cdp, rip ")
+	if userinput == ("help"):		
+		help = ("The commands include: ospf, ip, stp, vlans, passwords, port security, cdp, rip, speed, duplex ")
 		print(help)
 
 	reset = input("Need another command? (Y/N) ").lower()			#reset command after loop			
