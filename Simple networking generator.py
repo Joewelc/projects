0c697a1e9ea1cb66c47202ba1a25aba7ddfa4cb4
Joewelc <101085294+Joewelc@users.noreply.github.com>
2022-03-20 15:27:24 -0700

    Update Simple networking generator.py

--- Simple networking generator.py
+++ Simple networking generator.py
@@ -4,7 +4,7 @@ def restart():
 		if True:
 			reset = main()
 	elif reset == str("n"):
-		reset = print("Good bye.")
+		reset = print("Good bye." "\n")
 		quit()
 	elif reset != str("y") or str("n"):
 		if True:
@@ -16,28 +16,28 @@ def main():
 	def ospfprocess():
 		try:
 			process_variable = int(input("Insert an OSPF process number between 1 and 65535: "))
+			ospf_area = int(input("Insert an ospf area between 0 and 4294967295: "))
 			if process_variable >= 1 and process_variable <= 65535:
-				return str(int(process_variable))
-		except ValueError:
-			print("Please use a number between 1 and 65535")
+				if ospf_area >= 0 and ospf_area <= 4294967295:
+					return str(process_variable), str(ospf_area)
+		except (ValueError, TypeError, AttributeError):
+			print("Invalid inputs, try again ")
 			ospfprocess()
 
 	if userinput == ("ospf"):	
-		if userinput == ("ospf"):	
-			ospfval = ospfprocess()						
-			ospf_area_ask = input("Insert ospf area: ")		
-			ospf_net = input("Insert the ospf network: ")	
-			ospf_mask = input("Insert the wildcard mask ")								
-			ospf_area = ("area " + ospf_area_ask)
-			ospf = ("\n" "Enable" "\n" "configure terminal" "\n" "router ospf " + str(ospfval) + "\n" "network " +ospf_net + " " + ospf_mask + " " +  ospf_area)
-			print(ospf)
+		ospf_pro_and_area = ospfprocess()							
+		ospf_net = input("Insert the ospf network: ")	
+		ospf_mask = input("Insert the wildcard mask ")								
+		ospf = ("\n" "enable" "\n" "configure terminal" "\n" "router ospf " + str(ospf_pro_and_area[0]) + "\n" "network " + ospf_net + " " + ospf_mask + " " + "area " + str(ospf_pro_and_area[1]))
+		print(ospf)
 		
-	if userinput == ("rip"): 	
-		rip = ("\n" "Enable" "\n" "configure terminal" "\n" "router rip " "\n" "network *IP subnet and mask* " "\n" "version 2" "\n")
+	if userinput == ("rip"):
+		ripnetwork = input("Insert ip subnet: ")
+		rip = ("\n" "enable" "\n" "configure terminal" "\n" "router rip " "\n" "network " + str(ripnetwork) + "\n" "version 2")
 		print(rip)
 
 	if userinput == ("stp"): 	
-		stp = ("\n" "Enable" "\n" "configure terminal" "\n" "spanning-tree mode rapid-pvst" "\n" "spanning tree vlan 1 root primary" "\n")
+		stp = ("\n" "enable" "\n" "configure terminal" "\n" "spanning-tree mode rapid-pvst" "\n" "spanning tree vlan 1 root primary" "\n")
 		print(stp)
 
 	if userinput == ("ip"):	
@@ -62,7 +62,7 @@ def main():
 
 	if userinput == ("vlan"):	
 		vlan_num = input("Insert a Vlan number: ")											
-		vlan = ("Enable" "\n" "configure terminal" "\n" "vlan " + vlan_num + "\n" "interface */*" "\n" "switchport mode access" "\n" "switchport access vlan " + vlan_num)
+		vlan = ("enable" "\n" "configure terminal" "\n" "vlan " + vlan_num + "\n" "interface */*" "\n" "switchport mode access" "\n" "switchport access vlan " + vlan_num)
 		print(vlan)
 
 	if userinput == ("password"): 
@@ -73,7 +73,7 @@ def main():
 			secret_enable = ("enable secret " + secret_ask + "\n")
 		elif secret_ask == str("n"):
 			secret_enable = ("")
-		password = ("Enable" "\n" "configure terminal" "\n" "enable password " + str(pass_input) + "\n" + str(secret_enable) + "service password-encryption")
+		password = ("\n" "enable" "\n" "configure terminal" "\n" "enable password " + str(pass_input) + "\n" + str(secret_enable) + "service password-encryption")
 		print(password)
 
 	if userinput == ("cdp"): 
@@ -82,7 +82,7 @@ def main():
 			cdp = ("cdp run" "\n" "Note: CDP is enabled by default")
 		elif cdp == str("disable"):
 			cdp = ("no cdp run")
-		print("Enable" "\n" "configure terminal" "\n" + cdp)
+		print("\n" "enable" "\n" "configure terminal" "\n" + cdp)
 
 	if userinput == ("port security"):	
 		port_sec_max = input("Max number of mac addresses allowed: ") 						
@@ -93,19 +93,19 @@ def main():
 			port_sec_mac = ("switchport port-security mac-address " + port_sec_mac_ask)
 		elif port_sec_opt == ("sticky"): 														
 			port_sec_mac = ("switchport port-security mac-address sticky ")
-		port_security = ("Enable" "\n" "configure terminal" "\n" "interface */*" "\n" "switchport mode access" "\n" "switchport port-security " "\n" + port_sec_maxmac + "\n" + str(port_sec_mac))
+		port_security = ("\n" "enable" "\n" "configure terminal" "\n" "interface */*" "\n" "switchport mode access" "\n" "switchport port-security " "\n" + port_sec_maxmac + "\n" + str(port_sec_mac))
 		print(port_security)
 
 	if userinput == ("speed"):	
-		speed = ("\n" "Enable" "\n" "configure terminal" "\n" "interface */*" "\n" "speed 100" "\n")
+		speed = ("\n" "enable" "\n" "configure terminal" "\n" "interface */*" "\n" "speed 100" "\n")
 		print(speed)
 
 	if userinput == ("duplex"):	
-		duplex = ("\n" "Enable" "\n" "configure terminal" "\n" "interface */*" "duplex full" "\n")
+		duplex = ("\n" "enable" "\n" "configure terminal" "\n" "interface */*" "duplex full" "\n")
 		print(duplex)
 
 	if userinput == ("portfast"):
-		portfast = ("Enable" "\n" "configure terminal" "\n" "spanning-tree portfast default ")
+		portfast = ("enable" "\n" "configure terminal" "\n" "spanning-tree portfast default ")
 		print(portfast)
 
 	if userinput == ("help"):		
